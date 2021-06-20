1b6a96216025122f7277a96d04f9e14aa9578064
rhishikesh <rhishikesh.akole@gmail.com>
2021-06-20 13:25:06 +0530

    init commit

---
+++ src/test/java/com/dhenu_naturals/invoice_generator/InvoiceGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhenu_naturals.invoice_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
